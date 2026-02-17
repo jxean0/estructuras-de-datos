e90dfec6e37bde6425779d3dbcdddf8f48164a3e
jxean0 <jxean0@gmail.com>
2026-02-17 00:52:21 -0500

    Replace ListaRepuestos with ejemplo3 class

--- ejemplos/ejemplo3.java
+++ ejemplos/ejemplo3.java
@@ -1,18 +1,11 @@
-import java.util.ArrayList;
-
-public class ListaRepuestos {
+public class ejemplo3 {
     public static void main(String[] args) {
-        ArrayList<String> repuestos = new ArrayList<>();
-        repuestos.add("Filtro de aceite");
-        repuestos.add("Pastillas de freno");
-        repuestos.add("Bujías");
-        repuestos.add("Batería");
-        repuestos.add("Correa de distribución");
-        
-        for (String repuesto : repuestos) {
-            System.out.println(repuesto);
-        }
-        
-        System.out.println("Total de repuestos: " + repuestos.size());
+        String[] carros = {"Toyota", "Mazda", "Chevrolet", "Ford", "Honda"};
+        System.out.println("Los carros son:");
+        System.out.println("Posición 0: " + carros[0]);
+        System.out.println("Posición 1: " + carros[1]);
+        System.out.println("Posición 2: " + carros[2]);
+        System.out.println("Posición 3: " + carros[3]);
+        System.out.println("Posición 4: " + carros[4]);
     }
 }
